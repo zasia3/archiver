7e86f2ae4999e226b8671a03dc2a644594c5b62d
Joanna Zatorska <joanna.zatorska@thesoftwarehouse.pl>
2017-06-06 16:41:40 +0200

    added alamofire and rxswift

---
+++ Network/Network.swift
@@ -0,0 +1,10 @@
+//
+//  Network.swift
+//  Archiver
+//
+//  Created by Joanna Zatorska on 06/06/2017.
+//  Copyright © 2017 Joanna Zatorska. All rights reserved.
+//
+
+import Foundation
+import Alamofire
---
+++ RxAlamofire/RxAlamofire.h
@@ -0,0 +1,19 @@
+//
+//  RxAlamofire.h
+//  RxAlamofire
+//
+//  Created by Joanna Zatorska on 06/06/2017.
+//  Copyright © 2017 Joanna Zatorska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxAlamofire.
+FOUNDATION_EXPORT double RxAlamofireVersionNumber;
+
+//! Project version string for RxAlamofire.
+FOUNDATION_EXPORT const unsigned char RxAlamofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxAlamofire/PublicHeader.h>
+
+
